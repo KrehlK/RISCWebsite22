3120bb4d75dc83917b04324aefd55712572e2266
KrehlK <81425394+KrehlK@users.noreply.github.com>
2022-06-29 15:33:12 -0400

    Add files via upload

---
+++ Risc_website/index.js
@@ -0,0 +1,39 @@
+var password = "Risc90388547";
+var login_attempts = 3;
+
+// function passcheck() {
+//   if(document.getElementById('pass1').value != password){
+//     alert('Wrong Password, Try again.');
+//     return false;
+//   }
+
+//   if(document.getElementById('pass1').value == password) {
+//     alert('Correct Password. Click OK to enter webpage')
+//   }
+// }
+
+function check_form() {
+  var pass = document.getElementById("pass").value;
+  if (pass === password) {
+    alert("SuccessFully Logged In");
+    document.getElementById("pass").value = "";
+  }
+  else {
+    if (login_attempts == 0) {
+      alert("No Login Attempts Available");
+      return false
+    }
+    else {
+      login_attempts = login_attempts - 1;
+      alert("Login Failed Now Only " + login_attempts + " Login Attempts Available");
+      if (login_attempts == 0) {
+        document.getElementById("pass").disabled = true;
+        document.getElementById("form1").disabled = true;
+      }
+
+      return false;
+    }
+  }
+
+  return false;
+}	
---
+++ Risc_website/website.js
@@ -0,0 +1,15 @@
+
+var username = document.getElementById("username");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+
+function addAccount(){
+  
+  console.log("hello");
+  return false;
+} 
+
+
+
+
+
